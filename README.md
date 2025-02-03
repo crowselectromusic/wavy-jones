544cc8527b17298d2d6f3763812f7c29cf486648
Cory Alder <cory@davander.com>
2025-02-03 15:12:31 -0800

    Updated readme and refactored scope code

--- README.md
+++ README.md
@@ -1,4 +1,87 @@
 Wavy Jones
 ==========
 
-Wavy Jones is a simple wave of adding an oscilloscope to your Web Audio project. Read how to get started with it here - http://stuartmemo.com/wavy-jones/
+Wavy Jones is a simple way of adding an oscilloscope to your Web Audio project. It was originally written by [Stuart Memo](http://stuartmemo.com/wavy-jones/) back in 2013 - this is a slightly modernized fork for use on the [Crows Electromusic website](https://crowselectromusic.com) - it's key feature is being easier to use from [Tone.js](https://tonejs.github.io/).
+
+Usage:
+
+1. Reference Wavy Jones, after downloading it, by doing something like this:
+
+```<script src="/wavy-jones.js"></script>```
+
+2. Make somewhere to show your oscillator - create and empty div and give it an id like so:
+
+```<div id="oscilloscope"></div>```
+
+Make sure to give it a height and width!
+
+```
+<style>
+    #oscilloscope {
+        width: 100%;
+        height: 120px;
+    }
+</style>
+```
+
+3. Connect it to an audio source. Wavy Jones is basically a normal Audio Analyser node wearing a sparkly jacket. This means you can connect it up just like you would any other audio node.
+
+```
+// create a wavyjones instance on the 'oscilloscope' element.
+let wavy = new WavyJones('oscilloscope');
+
+// create an audio context
+let context = new AudioContext()
+
+// get wavy set up in the context
+wavy.setupInAudioContext(context);
+
+// create a sinewave oscillator in the audio context
+let sineWave = context.createOscillator()
+
+// tell wavy to analyze the sineWave
+sineWave.connect(wavy.analyzer);
+
+// connect the sineWave to audio, so we can hear it too
+sineWave.connect(context.destination);
+```
+
+## Tone.js
+To use this with [Tone.js](https://tonejs.github.io/) is pretty simple:
+
+```
+    // create an oscillator, routed to the destination
+    const osc = new Tone.Oscillator().toDestination();
+    osc.frequency.value = "C4";
+
+    // this has to be a user-triggered event, or the browser won't let it happen.
+    function startScope() { 
+        osc.start("+0");
+        if (window.scope.analyser == null) {
+            let context = Tone.getContext().rawContext;
+            window.scope.setupInAudioContext(context);
+            osc.connect(window.scope.analyser);
+        }
+    }
+
+    // when the page loads, add a scope
+    ready(()=>{
+        window.scope = new WavyJones('oscilloscope');
+    });
+
+    // a little document ready function from https://youmightnotneedjquery.com/#ready
+    function ready(fn) {
+        if (document.readyState !== 'loading') {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+    // if the window resizes, resize the scope
+    window.addEventListener('resize', function(event) {
+        if (window.scope) {
+            window.scope.updateCanvasSize();
+        }
+    }, true);
+```
--- wavy-jones.js
+++ wavy-jones.js
@@ -1,57 +1,83 @@
-var WavyJones = function (context, elem) {
-  var analyser = context.createAnalyser();
-  var elem = document.getElementById(elem);
+class WavyJones {
+  constructor(element, lineThickness = 3, lineColor = "#43a047") {
+    let elem = document.getElementById(element);
+
+    // set up an svg inside the target element
+    let svgNamespace = "http://www.w3.org/2000/svg";
+    let paper = document.createElementNS(svgNamespace, "svg");
+
+    paper.setAttribute('width', elem.offsetWidth - Math.round(lineThickness*1.5));
+    paper.setAttribute('height', elem.offsetHeight);
+    paper.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+    elem.appendChild(paper); 
+
+    // add a path for our oscilloscope line to the svg
+    let oscLine = document.createElementNS(svgNamespace, "path");
+    oscLine.setAttribute("stroke", lineColor);
+    oscLine.setAttribute("stroke-width", lineThickness);
+    oscLine.setAttribute("fill","none");
+    paper.appendChild(oscLine);
+
+    // draw an initial line
+    oscLine.setAttribute("d", `M 0,${elem.offsetHeight/2} L${elem.offsetWidth},${elem.offsetHeight/2} `);
+
+    // save these for access later
+    this.oscLine = oscLine;
+    this.paper = paper;
+    this.elem = elem;
+    this.lineColor = lineColor;
+    this.lineThickness = lineThickness;
+    this.analyser = null;
+  }
+
+  setupInAudioContext(context) {
+    let analyser = context.createAnalyser();
   
-  analyser.width = elem.offsetWidth;
-  analyser.height = elem.offsetHeight;
-  analyser.lineColor = 'yellow';
-  analyser.lineThickness = 5;
+    this.analyser = analyser;
 
-  var svgNamespace = "http://www.w3.org/2000/svg";
-  var paper = document.createElementNS(svgNamespace, "svg");
-  paper.setAttribute('width', analyser.width);
-  paper.setAttribute('height', analyser.height);
-  paper.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-  elem.appendChild(paper); 
+    this.updateCanvasSize();
 
-  var oscLine = document.createElementNS(svgNamespace, "path");
-  oscLine.setAttribute("stroke", analyser.lineColor);
-  oscLine.setAttribute("stroke-width", analyser.lineThickness);
-  oscLine.setAttribute("fill","none");
-  paper.appendChild(oscLine);
+    let noDataPoints = 10;
+    let freqData = new Uint8Array(analyser.frequencyBinCount);
+    let oscLine = this.oscLine;
 
-  var noDataPoints = 10,
-    freqData = new Uint8Array(analyser.frequencyBinCount);
+    let height = this.elem.offsetHeight;
 
+    let drawLine = function () {
+      analyser.getByteTimeDomainData(freqData);
 
-  var drawLine = function () {
-    analyser.getByteTimeDomainData(freqData);
+      let graphPoints = [];
+      let graphStr = '';
 
-    var graphPoints = [],
-      graphStr = '';
+      // move the line to the first data point
+      graphPoints.push('M0, ' + (freqData[0] / 128) * (height/2));
 
-    graphPoints.push('M0, ' + (analyser.height/2));
-
-    for (var i = 0; i < freqData.length; i++) {
-      if (i % noDataPoints) {
-        var point = (freqData[i] / 128) * (analyser.height / 2);
-        graphPoints.push('L' + i + ', ' + point); 
+      for (let i = 0; i < freqData.length; i++) {
+        if (i % noDataPoints) {
+          let point = (freqData[i] / 128) * (height / 2);
+          graphPoints.push('L' + i + ', ' + point); 
+        }
       }
-    }
 
-    for (i = 0; i < graphPoints.length; i++) {
-      graphStr += graphPoints[i];
-    }
+      for (let i = 0; i < graphPoints.length; i++) {
+        graphStr += graphPoints[i];
+      }
 
-    oscLine.setAttribute("stroke", analyser.lineColor);
-    oscLine.setAttribute("stroke-width", analyser.lineThickness);
+      oscLine.setAttribute("d", graphStr);
 
-    oscLine.setAttribute("d", graphStr);
+      setTimeout(drawLine, 100);
+    };
 
-    setTimeout(drawLine, 100);
-  };
+    drawLine();
+  }
 
-  drawLine();
+  updateStyle(lineColor, lineThickness) {
+    this.oscLine.setAttribute("stroke", lineColor);
+    this.oscLine.setAttribute("stroke-width", lineThickness);
+  }
 
-  return analyser;
-};
+  updateCanvasSize() {
+    this.paper.setAttribute('width', this.elem.offsetWidth - Math.round(this.lineThickness*1.5));
+    this.paper.setAttribute('height', this.elem.offsetHeight);
+  }
+}
